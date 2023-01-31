2c11c37bbd7c68362470ef17b1625132d567a378
Rogue Blue <rogueblueprint@protonmail.com>
2023-01-30 21:07:16 -0700

    Added assignment solution for intro to php

---
+++ Intro-To-PHP/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Sivnerof PHP</title>
+</head>
+<body>
+	<h1>Sivnerof PHP</h1>
+	<pre>
+          _____          
+         /\    \         
+        /::\    \        
+       /::::\    \       
+      /::::::\    \      
+     /:::/\:::\    \     
+    /:::/__\:::\    \    
+    \:::\   \:::\    \   
+  ___\:::\   \:::\    \  
+ /\   \:::\   \:::\    \ 
+/::\   \:::\   \:::\____\
+\:::\   \:::\   \::/    /
+ \:::\   \:::\   \/____/ 
+  \:::\   \:::\    \     
+   \:::\   \:::\____\    
+    \:::\  /:::/    /    
+     \:::\/:::/    /     
+      \::::::/    /      
+       \::::/    /       
+        \::/    /        
+         \/____/         
+                         
+	</pre>
+	<p>The SHA256 hash of "Sivnerof" is...</p>
+	<?php
+		echo hash('sha256', 'Sivnerof');
+	?>
+</body>
+</html>

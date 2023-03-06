493237317e9cb56cadfeb4d09b749c1d45bcc80e
Rogue Blue <rogueblueprint@protonmail.com>
2023-03-05 19:13:44 -0700

    Added bootstrap file from starter code zip file

--- PHP-Forms/index.php
+++ PHP-Forms/index.php
@@ -5,10 +5,11 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Rock Paper Scissors | Home</title>
+        <?php require_once "bootstrap.php";?>
     </head>
     <body>
         <div class="container">
-            <h1>Welcome to Rock Paper Scissors</h1>
+            <h1>Welcome To Rock Paper Scissors</h1>
             <p><a href="login.php">Please Log In</a></p>
             <p>Attempt to go to <a href="game.php">game.php</a> without logging in - it should fail with an error message.</p>
         </div>
